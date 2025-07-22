c7ef44c88455c9ce2c23cd6164e981ce7a10aba8
nicesoft-labs <me@sbelikov.ru>
2025-07-22 21:20:35 +0300

    Update main.py

--- photon_installer/main.py
+++ photon_installer/main.py
@@ -3,27 +3,111 @@
 # * SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-only
 # */
 from os.path import dirname, join
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import sys
 import traceback
 from commandutils import CommandUtils
 import yaml
 
+class RussianArgumentParser(ArgumentParser):
+    """Класс парсера аргументов, выводящий сообщения об ошибках на русском."""
+
+    def error(self, message):
+        sys.stderr.write(f"Ошибка: {message}\n\n")
+        self.print_help()
+        sys.exit(2)
 
 def main():
-    parser = ArgumentParser()
-    parser.add_argument("-i", "--image-type", dest="image_type")
-    parser.add_argument("-c", "--install-config", dest="install_config_file")
-    parser.add_argument("-u", "--ui-config", dest="ui_config_file")
+    parser = RussianArgumentParser(
+        description=(
+            "Установщик NiceOS. Позволяет создавать ISO-образ или выполнить "
+            "установку из локального репозитория."
+        ),
+        epilog=(
+            "Пример:\n"
+            "  python3 main.py -c sample_ks.cfg -r /path/to/repo -w /mnt/niceos-root -v 5.2"
+        ),
+        formatter_class=ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "-i",
+        "--image-type",
+        dest="image_type",
+        help="Тип создаваемого образа (например, iso)",
+    )
+    parser.add_argument(
+        "-c",
+        "--install-config",
+        dest="install_config_file",
+        help="Путь к файлу конфигурации установки (kickstart)",
+    )
+    parser.add_argument(
+        "-u",
+        "--ui-config",
+        dest="ui_config_file",
+        help="Файл настроек графического интерфейса установщика",
+    )
     # comma separated paths to rpms
-    parser.add_argument("-r", "--repo-paths", dest="repo_paths", default=None)
-    parser.add_argument("-o", "--options-file", dest="options_file")
-    parser.add_argument("-w", "--working-directory", dest="working_directory")
-    parser.add_argument("-l", "--log-path", dest="log_path", default="/var/log")
-    parser.add_argument("-e", "--eula-file", dest="eula_file_path", default=None)
-    parser.add_argument("-t", "--license-title", dest="license_display_title", default=None)
-    parser.add_argument("-v", "--niceos-release-version", dest="niceos_release_version", required=True)
-    parser.add_argument("-p", "--param", dest='params', action='append', default=[])
+    parser.add_argument(
+        "-r",
+        "--repo-paths",
+        dest="repo_paths",
+        default=None,
+        help="Список путей к каталогам с RPM-пакетами, разделённый запятыми",
+    )
+    parser.add_argument(
+        "-o",
+        "--options-file",
+        dest="options_file",
+        help="JSON-файл с дополнительными опциями",
+    )
+    parser.add_argument(
+        "-w",
+        "--working-directory",
+        dest="working_directory",
+        help="Рабочая директория для установки системы",
+    )
+    parser.add_argument(
+        "-l",
+        "--log-path",
+        dest="log_path",
+        default="/var/log",
+        help="Каталог для размещения лог-файлов",
+    )
+    parser.add_argument(
+        "-e",
+        "--eula-file",
+        dest="eula_file_path",
+        default=None,
+        help="Путь к файлу лицензионного соглашения (EULA)",
+    )
+    parser.add_argument(
+        "-t",
+        "--license-title",
+        dest="license_display_title",
+        default=None,
+        help="Заголовок окна отображения лицензии",
+    )
+    parser.add_argument(
+        "-v",
+        "--niceos-release-version",
+        dest="niceos_release_version",
+        required=True,
+        help="Версия релиза NiceOS (обязательный аргумент)",
+    )
+    parser.add_argument(
+        "-p",
+        "--param",
+        dest="params",
+        action="append",
+        default=[],
+        metavar="KEY=VALUE",
+        help=(
+            "Дополнительный параметр в формате ключ=значение. "
+            "Может быть указан несколько раз"
+        ),
+    )
 
     options = parser.parse_args()
 
