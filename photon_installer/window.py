bcb8970cdc6debd61c0ddae70e3d0db334ac37ed
nicesoft-labs <me@sbelikov.ru>
2025-07-23 13:46:56 +0300

    Update window.py

--- photon_installer/window.py
+++ photon_installer/window.py
@@ -12,9 +12,9 @@ from action import Action
 
 class Window(Action):
 
-    def __init__(self, height, width, maxy, maxx, title, can_go_back,
-                 action_panel=None, items=None, menu_helper=None, position=0,
-                 tab_enabled=True, can_go_next=False, read_text=False):
+     def __init__(self, height, width, maxy, maxx, title, can_go_back, action_panel=None, items=None,
+                 menu_helper=None, position=0, tab_enabled=True, can_go_next=False, read_text=False, logger=None):
+        self.logger = logger
         self.can_go_back = can_go_back
         self.can_go_next = can_go_next
         self.height = height
