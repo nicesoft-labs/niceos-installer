09c169ec950a64cdf83b0add168010c9d211f483
nicesoft-labs <me@sbelikov.ru>
2025-07-23 21:26:23 +0300

    Update readtext.py

--- photon_installer/readtext.py
+++ photon_installer/readtext.py
@@ -102,10 +102,15 @@ class ReadText(Action):
                         conf_message_height = 8
                         conf_message_width = 48
                         conf_message_button_y = (self.maxy - conf_message_height) // 2 + 5
-                        confrim_window = ConfirmWindow(conf_message_height, conf_message_width,
-                                                       self.maxy,
-                                                       self.maxx, conf_message_button_y,
-                                                       self.confirmation_error_msg, True)
+                        confrim_window = ConfirmWindow(
+                            conf_message_height,
+                            conf_message_width,
+                            self.maxy,
+                            self.maxx,
+                            conf_message_button_y,
+                            self.confirmation_error_msg,
+                            info=True,
+                        )
                         confrim_window.do_action()
                         return ActionResult(False, {'goBack': True})
                     self.set_field()
