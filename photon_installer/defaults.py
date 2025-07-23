276521e088bb93791030301ce8a42043b4140a9e
nicesoft-labs <me@sbelikov.ru>
2025-07-23 12:06:43 +0300

    Update defaults.py

--- photon_installer/defaults.py
+++ photon_installer/defaults.py
@@ -1,13 +1,17 @@
-#/*
-# * Copyright © 2020-2023 VMware, Inc.
-# * SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-only
-# */
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+© 2025 ООО "НАЙС СОФТ ГРУПП" (ИНН 5024245440)
+Контакты: <niceos@ncsgp.ru>
+"""
+
 import os
 
 class Defaults():
-    WORKING_DIRECTORY = "/mnt/photon-root"
+    WORKING_DIRECTORY = "/mnt/niceos-root"
     MOUNT_PATH = "/mnt/media"
     REPO_PATHS = MOUNT_PATH + "/RPMS"
     LOG_PATH = "/var/log"
     INSECURE_INSTALLATION = False
-    PHOTON_RELEASE_VERSION = "5.0"
+    NICEOS_RELEASE_VERSION = "5.2"
