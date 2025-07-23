c7bf5114c9d540b190ad5478db0cbca0f52898c3
nicesoft-labs <me@sbelikov.ru>
2025-07-23 11:45:31 +0300

    Update __init__.py

--- photon_installer/__init__.py
+++ photon_installer/__init__.py
@@ -1,11 +1,18 @@
-#
-# Copyright © 2020-2021 VMware, Inc.
-# SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-only
-#
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+© 2025 ООО "НАЙС СОФТ ГРУПП" (ИНН 5024245440)
+Контакты: <niceos@ncsgp.ru>
+
+Описание:
+Модуль для добавления публичного ключа и разрешения root-доступа по SSH
+в системе NiceOS в процессе пост-установки.
+"""
 
 import sys
 import glob
-import pkg_resources
+from importlib import metadata
 from os.path import dirname, basename, isfile, join
 
 
@@ -13,4 +20,7 @@ modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 sys.path.append(dirname(__file__))
 
-__version__ = pkg_resources.get_distribution(__name__).version
+try:
+    __version__ = metadata.version(__name__)
+except metadata.PackageNotFoundError:  # pragma: no cover - package not installed
+    __version__ = "0.0.0"
